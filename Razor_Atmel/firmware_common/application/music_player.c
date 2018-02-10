33d74bfeeebca4dd2afa8b956b1106848e096849
Ivan Chow <ivanchow23@hotmail.com>
2018-02-09 23:50:53 -0700

    Add algorithm from "buzzer advanced" module
    Ported the code from the buzzer advanced module to play
    "Heart and Soul". Changed variables to use underscores
    instead of Hungarian notation.

--- Razor_Atmel/firmware_common/application/music_player.c
+++ Razor_Atmel/firmware_common/application/music_player.c
@@ -25,6 +25,36 @@ Global variable definitions with scope limited to this local application.
 ***********************************************************************************************************************/
 static fnCode_type MusicPlayer_StateMachine;    /* The state machine function pointer */
 
+/* Note constants */
+static u16 au16_notes_right[]     = {F5, F5, F5, F5, F5, E5, D5, E5, F5, G5, A5, A5, A5, A5, A5, G5, F5, G5, A5, A5S, C6, F5, F5, D6, C6, A5S, A5, G5, F5, NO, NO};
+static u16 au16_duration_right[]  = {QN, QN, HN, EN, EN, EN, EN, EN, EN, QN, QN, QN, HN, EN, EN, EN, EN, EN, EN, QN,  HN, HN, EN, EN, EN, EN,  QN, QN, HN, HN, FN};
+static u16 au16_note_type_right[] = {RT, RT, HT, RT, RT, RT, RT, RT, RT, RT, RT, RT, HT, RT, RT, RT, RT, RT, RT, RT,  RT, HT, RT, RT, RT, RT,  RT, RT, RT, HT, HT};
+
+static u16 au16_notes_left[]     = {F4, F4, A4, A4, D4, D4, F4, F4, A3S, A3S, D4, D4, C4, C4, E4, E4};
+static u16 au16_duration_left[]  = {EN, EN, EN, EN, EN, EN, EN, EN, EN,  EN,  EN, EN, EN, EN, EN, EN};
+static u16 au16_note_type_left[] = {RT, RT, RT, RT, RT, RT, RT, RT, RT,  RT,  RT, RT, RT, RT, RT, RT};
+
+/* Right buzzer variables */
+static u8 u8_index_right = 0;
+static u32 u32_right_timer = 0;
+static u16 u16_current_duration_right = 0;
+static u16 u16_note_silent_duration_right = 0;
+static bool b_note_active_next_right = TRUE;
+
+/* Left buzzer variables */
+static u8 u8_index_left = 0;
+static u32 u32_left_timer = 0;
+static u16 u16_current_duration_left = 0;
+static u16 u16_note_silent_duration_left = 0;
+static bool b_note_active_next_left = TRUE;
+
+static u8 u8_current_index = 0;
+
+/***********************************************************************************************************************
+Local functions
+***********************************************************************************************************************/
+static void PlayNote(void);
+
 /***********************************************************************************************************************
 State Machine Declarations
 ***********************************************************************************************************************/
@@ -48,7 +78,6 @@ Description:
 */
 void MusicPlayerInitialize(void)
 {
-  DebugPrintf("Hello world!\n");
   MusicPlayer_StateMachine = MusicPlayerSM_Play;
 }
 
@@ -69,6 +98,147 @@ void MusicPlayerRunActiveState(void)
 /* Private functions                                                                                                  */
 /*--------------------------------------------------------------------------------------------------------------------*/
 
+/*----------------------------------------------------------------------------------------------------------------------
+Function: PlayNote
+
+Description:
+  Runs the algorithm that determines which note to play on each buzzer and for how long.
+  Adapted from the EiE "buzzers advanced" firmware module.
+*/
+static void PlayNote(void)
+{
+  // Right buzzer
+  if( IsTimeUp( &u32_right_timer, (u32)u16_current_duration_right) )
+  {
+    // Get the current time and note index
+    u32_right_timer = G_u32SystemTime1ms;
+    u8_current_index = u8_index_right;
+
+    // If the note is currently active
+    if( b_note_active_next_right )
+    {
+      // Check RT, ST, HT notes
+      if( au16_note_type_right[u8_current_index] == RT )
+      {
+        // RT notes have a short silent period
+        u16_current_duration_right = au16_duration_right[u8_current_index] - REGULAR_NOTE_ADJUSTMENT;
+        u16_note_silent_duration_right = REGULAR_NOTE_ADJUSTMENT;
+        b_note_active_next_right = FALSE;
+      }
+      else if( au16_note_type_right[u8_current_index] == ST )
+      {
+        // ST notes are brief, so they have a fixed note time
+        u16_current_duration_right = STACCATO_NOTE_TIME;
+        u16_note_silent_duration_right = au16_duration_right[u8_current_index] - STACCATO_NOTE_TIME;
+        b_note_active_next_right = FALSE;
+      }
+      else if( au16_note_type_right[u8_current_index] == HT )
+      {
+        // Hold notes don't have silent periosd - note is played for the full duration
+        u16_current_duration_right = au16_duration_right[u8_current_index];
+        u16_note_silent_duration_right = 0;
+        b_note_active_next_right = TRUE;
+
+        u8_index_right++;
+        if( u8_index_right == ( sizeof( au16_notes_right ) / sizeof( u16 ) ) )
+        {
+          u8_index_right = 0;
+        }
+      }
+
+      // Set the note frequency and turn the audio on or handle the special case of a "NO" note
+      if( au16_notes_right[u8_current_index] != NO )
+      {
+        PWMAudioSetFrequency( BUZZER1, au16_notes_right[u8_current_index] );
+        PWMAudioOn( BUZZER1 );
+      }
+      else
+      {
+        PWMAudioOff( BUZZER1 );
+      }
+    }
+    // Note is not active - silent period of the note
+    else
+    {
+      // Turn off the buzzer for this period
+      PWMAudioOff( BUZZER1 );
+
+      // Set currentDuration = silentDuration
+      u32_right_timer = G_u32SystemTime1ms;
+      u16_current_duration_right = u16_note_silent_duration_right;
+      b_note_active_next_right = TRUE;
+
+      u8_index_right++;
+      if( u8_index_right == ( sizeof( au16_notes_right ) / sizeof( u16 ) ) )
+      {
+        u8_index_right = 0;
+      }
+    }
+  }
+
+   // Left buzzer
+  if( IsTimeUp( &u32_left_timer, (u32)u16_current_duration_left ) )
+  {
+    u32_left_timer = G_u32SystemTime1ms;
+    u8_current_index = u8_index_left;
+
+    // Set up to play current note
+    if( b_note_active_next_left )
+    {
+      if( au16_note_type_left[u8_current_index] == RT )
+      {
+        u16_current_duration_left = au16_duration_left[u8_current_index] - REGULAR_NOTE_ADJUSTMENT;
+        u16_note_silent_duration_left = REGULAR_NOTE_ADJUSTMENT;
+        b_note_active_next_left = FALSE;
+      }
+
+      else if( au16_note_type_left[u8_current_index] == ST )
+      {
+        u16_current_duration_left = STACCATO_NOTE_TIME;
+        u16_note_silent_duration_left = au16_duration_left[u8_current_index] - STACCATO_NOTE_TIME;
+        b_note_active_next_left = FALSE;
+      }
+
+      else if( au16_note_type_left[u8_current_index] == HT )
+      {
+        u16_current_duration_left = au16_duration_left[u8_current_index];
+        u16_note_silent_duration_left = 0;
+        b_note_active_next_left = TRUE;
+
+        u8_index_left++;
+        if( u8_index_left == sizeof( au16_notes_left) / sizeof(u16) )
+        {
+          u8_index_left = 0;
+        }
+      }
+
+      // Set the buzzer frequency for the note (handle NO special case)
+      if( au16_notes_left[u8_current_index] != NO )
+      {
+        PWMAudioSetFrequency(BUZZER2, au16_notes_left[u8_current_index]);
+        PWMAudioOn( BUZZER2 );
+      }
+      else
+      {
+        PWMAudioOff( BUZZER2 );
+      }
+    }
+    else
+    {
+      PWMAudioOff( BUZZER2 );
+      u32_left_timer = G_u32SystemTime1ms;
+      u16_current_duration_left = u16_note_silent_duration_left;
+      b_note_active_next_left = TRUE;
+
+      u8_index_left++;
+      if( u8_index_left == sizeof( au16_notes_left ) / sizeof( u16 ) )
+      {
+        u8_index_left = 0;
+      }
+    } // end else if(b_note_active_next_left)
+  } // end if(IsTimeUp(&u32_left_timer, (u32)u16_current_duration_left))
+}
+
 /**********************************************************************************************************************
 State Machine Function Definitions
 **********************************************************************************************************************/
@@ -77,6 +247,8 @@ State Machine Function Definitions
 /* Plays the current song. */
 static void MusicPlayerSM_Play(void)
 {
+  // Play the next note
+  PlayNote();
 }
 
 /*-------------------------------------------------------------------------------------------------------------------*/
