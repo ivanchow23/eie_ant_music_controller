f030c48d90a156b7eb60f65d01d1f085cf92b36c
Ivan Chow <ivanchow23@hotmail.com>
2018-02-11 22:08:49 -0700

    Data structure for holding song information
    Implemented data structure to hold information about a
    song. Each song will contain: title, artist, left note
    information, and right note information. Any song added
    in the near future will be added to a list.

--- Razor_Atmel/firmware_common/application/music_player.c
+++ Razor_Atmel/firmware_common/application/music_player.c
@@ -20,20 +20,49 @@ Existing variables (defined in other files -- should all contain the "extern" ke
 extern volatile u32 G_u32SystemTime1ms;         /* From board-specific source file */
 extern volatile u32 G_u32SystemTime1s;          /* From board-specific source file */
 
+/**********************************************************************************************************************
+Type Definitions
+**********************************************************************************************************************/
+/* Holds song information */
+typedef struct
+{
+  const char* title;
+  const char* artist;
+  const u16*  note_right;           /* Array of note frequencies for right buzzer */
+  const u16*  note_duration_right;  /* Array of note durations for right buzzer*/
+  const u16*  note_type_right;      /* Array of note types for right buzzer */
+  const u16   num_notes_right;      /* Number of notes for right buzzer */
+  const u16*  note_left;            /* Array of note frequencies for left buzzer */
+  const u16*  note_duration_left;   /* Array of note durations for left buzzer*/
+  const u16*  note_type_left;       /* Array of note types for left buzzer */
+  const u16   num_notes_left;       /* Number of notes for left buzzer */
+} SongInfoType;
+
 /***********************************************************************************************************************
 Global variable definitions with scope limited to this local application.
 ***********************************************************************************************************************/
 static fnCode_type MusicPlayer_StateMachine;    /* The state machine function pointer */
 
-/* Note constants */
-static u16 au16_notes_right[]     = {F5, F5, F5, F5, F5, E5, D5, E5, F5, G5, A5, A5, A5, A5, A5, G5, F5, G5, A5, A5S, C6, F5, F5, D6, C6, A5S, A5, G5, F5, NO, NO};
-static u16 au16_duration_right[]  = {QN, QN, HN, EN, EN, EN, EN, EN, EN, QN, QN, QN, HN, EN, EN, EN, EN, EN, EN, QN,  HN, HN, EN, EN, EN, EN,  QN, QN, HN, HN, FN};
-static u16 au16_note_type_right[] = {RT, RT, HT, RT, RT, RT, RT, RT, RT, RT, RT, RT, HT, RT, RT, RT, RT, RT, RT, RT,  RT, HT, RT, RT, RT, RT,  RT, RT, RT, HT, HT};
+/* Song #1 */
+static const u16 song1_note_right[]          = { F5, F5, F5, F5, F5, E5, D5, E5, F5, G5, A5, A5, A5, A5, A5, G5, F5, G5, A5, A5S, C6, F5, F5, D6, C6, A5S, A5, G5, F5, NO, NO };
+static const u16 song1_note_duration_right[] = { QN, QN, HN, EN, EN, EN, EN, EN, EN, QN, QN, QN, HN, EN, EN, EN, EN, EN, EN, QN,  HN, HN, EN, EN, EN, EN,  QN, QN, HN, HN, FN };
+static const u16 song1_note_type_right[]     = { RT, RT, HT, RT, RT, RT, RT, RT, RT, RT, RT, RT, HT, RT, RT, RT, RT, RT, RT, RT,  RT, HT, RT, RT, RT, RT,  RT, RT, RT, HT, HT };
+static const u16 song1_note_left[]           = { F4, F4, A4, A4, D4, D4, F4, F4, A3S, A3S, D4, D4, C4, C4, E4, E4 };
+static const u16 song1_note_duration_left[]  = { EN, EN, EN, EN, EN, EN, EN, EN, EN,  EN,  EN, EN, EN, EN, EN, EN };
+static const u16 song1_note_type_left[]      = { RT, RT, RT, RT, RT, RT, RT, RT, RT,  RT,  RT, RT, RT, RT, RT, RT };
 
-static u16 au16_notes_left[]     = {F4, F4, A4, A4, D4, D4, F4, F4, A3S, A3S, D4, D4, C4, C4, E4, E4};
-static u16 au16_duration_left[]  = {EN, EN, EN, EN, EN, EN, EN, EN, EN,  EN,  EN, EN, EN, EN, EN, EN};
-static u16 au16_note_type_left[] = {RT, RT, RT, RT, RT, RT, RT, RT, RT,  RT,  RT, RT, RT, RT, RT, RT};
+static const SongInfoType song1 = { "Heart and Soul", "Hoagy Carmichael",
+                                    song1_note_right, song1_note_duration_right, song1_note_type_right, sizeof( song1_note_right ) / sizeof( song1_note_right[0] ),
+                                    song1_note_left, song1_note_duration_left, song1_note_type_left, sizeof( song1_note_left ) / sizeof( song1_note_left[0] )
+                                  };
 
+/* List of songs */
+static const SongInfoType* song_list[] = { &song1 };
+
+/* Index of the current song */
+static u8 current_song_index = 0;
+
+// TODO: IC - need a function that resets these variables when song is changed
 /* Right buzzer variables */
 static u8 u8_index_right = 0;
 static u32 u32_right_timer = 0;
@@ -47,8 +76,6 @@ static u32 u32_left_timer = 0;
 static u16 u16_current_duration_left = 0;
 static u16 u16_note_silent_duration_left = 0;
 static bool b_note_active_next_left = TRUE;
-
-static u8 u8_current_index = 0;
 
 /***********************************************************************************************************************
 Local functions
@@ -108,48 +135,48 @@ Description:
 static void PlayNote(void)
 {
   // Right buzzer
-  if( IsTimeUp( &u32_right_timer, (u32)u16_current_duration_right) )
+  if( IsTimeUp( &u32_right_timer, (u32)u16_current_duration_right ) )
   {
     // Get the current time and note index
     u32_right_timer = G_u32SystemTime1ms;
-    u8_current_index = u8_index_right;
+    u8 u8_current_index = u8_index_right;
 
     // If the note is currently active
     if( b_note_active_next_right )
     {
       // Check RT, ST, HT notes
-      if( au16_note_type_right[u8_current_index] == RT )
+      if( song_list[current_song_index]->note_type_right[u8_current_index] == RT )
       {
         // RT notes have a short silent period
-        u16_current_duration_right = au16_duration_right[u8_current_index] - REGULAR_NOTE_ADJUSTMENT;
+        u16_current_duration_right = song_list[current_song_index]->note_duration_right[u8_current_index] - REGULAR_NOTE_ADJUSTMENT;
         u16_note_silent_duration_right = REGULAR_NOTE_ADJUSTMENT;
         b_note_active_next_right = FALSE;
       }
-      else if( au16_note_type_right[u8_current_index] == ST )
+      else if( song_list[current_song_index]->note_type_right[u8_current_index] == ST )
       {
         // ST notes are brief, so they have a fixed note time
         u16_current_duration_right = STACCATO_NOTE_TIME;
-        u16_note_silent_duration_right = au16_duration_right[u8_current_index] - STACCATO_NOTE_TIME;
+        u16_note_silent_duration_right = song_list[current_song_index]->note_duration_right[u8_current_index] - STACCATO_NOTE_TIME;
         b_note_active_next_right = FALSE;
       }
-      else if( au16_note_type_right[u8_current_index] == HT )
+      else if( song_list[current_song_index]->note_type_right[u8_current_index] == HT )
       {
         // Hold notes don't have silent periosd - note is played for the full duration
-        u16_current_duration_right = au16_duration_right[u8_current_index];
+        u16_current_duration_right = song_list[current_song_index]->note_duration_right[u8_current_index];
         u16_note_silent_duration_right = 0;
         b_note_active_next_right = TRUE;
 
         u8_index_right++;
-        if( u8_index_right == ( sizeof( au16_notes_right ) / sizeof( u16 ) ) )
+        if( u8_index_right >= song_list[current_song_index]->num_notes_right )
         {
           u8_index_right = 0;
         }
       }
 
       // Set the note frequency and turn the audio on or handle the special case of a "NO" note
-      if( au16_notes_right[u8_current_index] != NO )
+      if( song_list[current_song_index]->note_right[u8_current_index] != NO )
       {
-        PWMAudioSetFrequency( BUZZER1, au16_notes_right[u8_current_index] );
+        PWMAudioSetFrequency( BUZZER1, song_list[current_song_index]->note_right[u8_current_index] );
         PWMAudioOn( BUZZER1 );
       }
       else
@@ -169,53 +196,53 @@ static void PlayNote(void)
       b_note_active_next_right = TRUE;
 
       u8_index_right++;
-      if( u8_index_right == ( sizeof( au16_notes_right ) / sizeof( u16 ) ) )
+      if( u8_index_right >= song_list[current_song_index]->num_notes_right )
       {
         u8_index_right = 0;
       }
     }
   }
 
-   // Left buzzer
+  // Left buzzer
   if( IsTimeUp( &u32_left_timer, (u32)u16_current_duration_left ) )
   {
     u32_left_timer = G_u32SystemTime1ms;
-    u8_current_index = u8_index_left;
+    u8 u8_current_index = u8_index_left;
 
     // Set up to play current note
     if( b_note_active_next_left )
     {
-      if( au16_note_type_left[u8_current_index] == RT )
+      if( song_list[current_song_index]->note_type_left[u8_current_index] == RT )
       {
-        u16_current_duration_left = au16_duration_left[u8_current_index] - REGULAR_NOTE_ADJUSTMENT;
+        u16_current_duration_left = song_list[current_song_index]->note_duration_left[u8_current_index] - REGULAR_NOTE_ADJUSTMENT;
         u16_note_silent_duration_left = REGULAR_NOTE_ADJUSTMENT;
         b_note_active_next_left = FALSE;
       }
 
-      else if( au16_note_type_left[u8_current_index] == ST )
+      else if( song_list[current_song_index]->note_type_left[u8_current_index] == ST )
       {
         u16_current_duration_left = STACCATO_NOTE_TIME;
-        u16_note_silent_duration_left = au16_duration_left[u8_current_index] - STACCATO_NOTE_TIME;
+        u16_note_silent_duration_left = song_list[current_song_index]->note_duration_left[u8_current_index] - STACCATO_NOTE_TIME;
         b_note_active_next_left = FALSE;
       }
 
-      else if( au16_note_type_left[u8_current_index] == HT )
+      else if( song_list[current_song_index]->note_type_left[u8_current_index] == HT )
       {
-        u16_current_duration_left = au16_duration_left[u8_current_index];
+        u16_current_duration_left = song_list[current_song_index]->note_duration_left[u8_current_index];
         u16_note_silent_duration_left = 0;
         b_note_active_next_left = TRUE;
 
         u8_index_left++;
-        if( u8_index_left == sizeof( au16_notes_left) / sizeof(u16) )
+        if( u8_index_left >= song_list[current_song_index]->num_notes_left )
         {
           u8_index_left = 0;
         }
       }
 
       // Set the buzzer frequency for the note (handle NO special case)
-      if( au16_notes_left[u8_current_index] != NO )
+      if( song_list[current_song_index]->note_left[u8_current_index] != NO )
       {
-        PWMAudioSetFrequency(BUZZER2, au16_notes_left[u8_current_index]);
+        PWMAudioSetFrequency( BUZZER2, song_list[current_song_index]->note_left[u8_current_index] );
         PWMAudioOn( BUZZER2 );
       }
       else
@@ -231,7 +258,7 @@ static void PlayNote(void)
       b_note_active_next_left = TRUE;
 
       u8_index_left++;
-      if( u8_index_left == sizeof( au16_notes_left ) / sizeof( u16 ) )
+      if( u8_index_left >= song_list[current_song_index]->num_notes_left )
       {
         u8_index_left = 0;
       }
--- Razor_Atmel/firmware_common/application/music_player.h
+++ Razor_Atmel/firmware_common/application/music_player.h
@@ -1,19 +1,6 @@
 /**********************************************************************************************************************
 File: music_player.h
-
-----------------------------------------------------------------------------------------------------------------------
-To start a new task using this user_app1 as a template:
-1. Follow the instructions at the top of user_app1.c
-2. Use ctrl-h to find and replace all instances of "user_app1" with "yournewtaskname"
-3. Use ctrl-h to find and replace all instances of "UserApp1" with "YourNewTaskName"
-4. Use ctrl-h to find and replace all instances of "USER_APP1" with "YOUR_NEW_TASK_NAME"
-5. Add #include yournewtaskname.h" to configuration.h
-6. Add/update any special configurations required in configuration.h (e.g. peripheral assignment and setup values)
-7. Delete this text (between the dashed lines)
-----------------------------------------------------------------------------------------------------------------------
-
 Description: Header file music_player.c
-
 **********************************************************************************************************************/
 
 #ifndef __MUSIC_PLAYER_H
