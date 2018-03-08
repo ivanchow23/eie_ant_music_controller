a09de3ae970ecefcf313035b6b9ad8cb441dfaae
Ivan Chow <ivanchow23@hotmail.com>
2018-03-07 20:35:15 -0700

    [MIDI] Add default tempo to track splitter script
    Search through input MIDI file for a "default" tempo.
    This is to fix cases where the original input track does
    not have a tempo message, which caused the output split
    tracks to default back to 500000 us/beat and not be in
    sync with other tracks.

--- Midi_Parser/midi_track_splitter.py
+++ Midi_Parser/midi_track_splitter.py
@@ -6,12 +6,18 @@
 import argparse
 import os
 import mido
-from mido import MidiFile, MidiTrack
+from mido import MidiFile, MidiTrack, MetaMessage
 
-# Parses the input mide file into separate MIDI tracks
+# Parses the input midi file into separate MIDI tracks
 def parse_midi(mid_file):
     in_mid = MidiFile(mid_file)
     in_mid_basename = os.path.splitext(mid_file)[0]
+
+    # Find the tempo of the MIDI file
+    # Note: This is not ideal because there could be changes in tempo during the song
+    #       However, this is to try giving tracks without the tempo messages consistency with other tracks that do
+    base_tempo = find_tempo(in_mid)
+    print('Using {} us/beat as tempo for tracks without a tempo message.\n'.format(base_tempo))
 
     # Iterate through each track in file
     for i, in_track in enumerate(in_mid.tracks):
@@ -22,6 +28,11 @@ def parse_midi(mid_file):
 
         # Make ticks per beat consistent
         out_mid.ticks_per_beat = in_mid.ticks_per_beat
+
+        # Check if this current track has a tempo message in it
+        # This is so original tracks that don't have a tempo message gets one
+        if not has_tempo(in_track):
+            out_track.append(MetaMessage('set_tempo', tempo=base_tempo))
 
         print('Track {}: {}'.format(i, in_track.name))
 
@@ -36,12 +47,31 @@ def parse_midi(mid_file):
         except IOError:
             print("Could not save file: " + out_mid_file_name)
 
+# A fairly inefficient way to get the first tempo message of the entire MIDI file
+def find_tempo(midi_file):
+    for i, track in enumerate(midi_file.tracks):
+        for msg in track:
+            msg_dict = msg.dict()
+
+            if 'tempo' in msg_dict:
+                return msg_dict['tempo']
+
+# A fairly inefficient way to check if a track already has a tempo message in it
+def has_tempo(track):
+    for msg in track:
+        msg_dict = msg.dict()
+
+        if 'tempo' in msg_dict:
+            return True
+
+    return False
+
 # Parse arguments
 parser = argparse.ArgumentParser()
 parser.add_argument("mid_file", help="Input MIDI file to parse")
 args = parser.parse_args()
 
-print("\nParsing: " + args.mid_file + "\n")
+print("\nParsing: " + args.mid_file)
 
 # Parse the MIDI file
 parse_midi(args.mid_file)
